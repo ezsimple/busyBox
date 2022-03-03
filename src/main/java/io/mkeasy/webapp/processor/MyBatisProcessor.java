3140f257a9ba5cf61d2bd5f32886ad487c49b2be
mhlee <mkeasy@gmail.com>
2022-03-03 16:54:10 +0900

    support spring-jdbc 5.x

--- src/main/java/io/mkeasy/webapp/processor/MyBatisProcessor.java
+++ src/main/java/io/mkeasy/webapp/processor/MyBatisProcessor.java
@@ -1,34 +1,33 @@
 package io.mkeasy.webapp.processor;
 
-import io.mkeasy.utils.ListUtil;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.map.CaseInsensitiveMap;
-import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.session.SqlSession;
-import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedCaseInsensitiveMap;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class MyBatisProcessor implements ProcessorService {
-    private Map<String, List<MappedStatementInfo>> mappedStatementInfoMap = null;
+
+    @Autowired
     private SqlSession sqlSession;
 
-    private List<Object> EMPTY_LIST = Collections.emptyList();
+    // private List<Object> EMPTY_LIST = Collections.emptyList();
 
     public Object execute(ProcessorParam processorParam) throws Exception {
-        String path = processorParam.getQueryPath();
+        String path = processorParam.getNameSpace();
         CaseInsensitiveMap params = processorParam.getParams();
 
-        String queryPath = processorParam.getQueryPath();
-        String action = processorParam.getAction();
+        String queryPath = processorParam.getNameSpace();
+        String action = processorParam.getNameSpaceId();
         String returnId = queryPath + "." + action;
 
         Map<String, Object> resultSet = new LinkedCaseInsensitiveMap<Object>();
@@ -39,9 +38,9 @@ public class MyBatisProcessor implements ProcessorService {
 
         if (sqlCommandType == SqlCommandType.SELECT) {
             result = sqlSession.selectList(returnId, params);
-            if (result.equals(EMPTY_LIST)) { // 한개의 ROW가 모두 NULL일 경우
-                result = ListUtil.EMPTY_LIST;
-            }
+            // if (result.equals(EMPTY_LIST)) { // 한개의 ROW가 모두 NULL일 경우
+            //    result = ListUtil.EMPTY_LIST;
+            // }
         }
 
         if (sqlCommandType == SqlCommandType.INSERT) {
@@ -93,92 +92,8 @@ public class MyBatisProcessor implements ProcessorService {
         throw new Exception(sqlCommandType + " does not exist");
     }
 
-    class MappedStatementInfo {
-        boolean isSingleRow = false;
-        String id;
-        String returnId;
-        boolean isSelect = false;
-
-        public MappedStatementInfo(String id, String returnId, boolean isSelect, boolean isSingleRow) {
-            this.isSingleRow = isSingleRow;
-            this.id = id;
-            this.returnId = returnId;
-            this.isSelect = isSelect;
-        }
-    }
-
-    public List<MappedStatementInfo> getList(String path, String action) {
-        String key = path + "." + action;
-
-        if (mappedStatementInfoMap != null) {
-            List<MappedStatementInfo> result = mappedStatementInfoMap.get(key);
-            if (result != null)
-                return result;
-        }
-
-        if (sqlSession == null) {
-            sqlSession = (SqlSession) ProcessorServiceFactory.getBean(SqlSessionTemplate.class);
-        }
-
-        List<String> idList = new ArrayList<String>();
-        Map<String, List<MappedStatementInfo>> msInfoMap = new LinkedCaseInsensitiveMap<List<MappedStatementInfo>>();
-
-        Collection<String> collection = sqlSession.getConfiguration().getMappedStatementNames();
-
-        for (String id : collection) {
-            if (!StringUtils.contains(id, ".")) continue;
-            idList.add(id);
-        }
-
-        String[] ids = idList.toArray(new String[0]);
-        Arrays.sort(ids);
-
-        for (String id : ids) {
-            boolean isSingleRow = false;
-            boolean isSelect = false;
-            String keyId = id;
-            String returnId = id;
-            MappedStatement mappedStatement = null;
-
-            try {
-                mappedStatement = sqlSession.getConfiguration().getMappedStatement(id);
-            } catch (Exception e) {
-                continue;
-            }
-
-            if (!id.equals(mappedStatement.getId())) {
-                continue;
-            }
-
-            //mappedStatement.getConfiguration().
-            if (mappedStatement.getSqlCommandType() == SqlCommandType.SELECT) {
-                isSelect = true;
-            }
-
-            MappedStatementInfo msi = new MappedStatementInfo(id, returnId, isSelect, isSingleRow);
-
-            List<MappedStatementInfo> list = msInfoMap.get(keyId);
-
-            if (list == null) {
-                list = new ArrayList<MyBatisProcessor.MappedStatementInfo>();
-                msInfoMap.put(keyId, list);
-            }
-            list.add(msi);
-        }
-
-        mappedStatementInfoMap = msInfoMap;
-        ProcessorServiceFactory.setMyBatisMappedStatementInfoMap(mappedStatementInfoMap);
-
-        return mappedStatementInfoMap.get(key);
-    }
-
     public SqlCommandType getSqlCommandType(String path, String action) {
         String returnId = path + "." + action;
-
-        if (sqlSession == null) {
-            sqlSession = (SqlSession) ProcessorServiceFactory.getBean(SqlSessionTemplate.class);
-        }
-
         Collection<String> collection = sqlSession.getConfiguration().getMappedStatementNames();
         if (!collection.contains(returnId)) {
             log.error("returnId = {} does not exist in MappedStatementNames", returnId);
