68d4bd747b5c9db4d23d7823860c1149ca0c42d1
mhlee <mkeasy@gmail.com>
2022-03-03 21:46:38 +0900

    down sizing

--- src/main/java/io/mkeasy/webapp/processor/EsFactory.java
+++ src/main/java/io/mkeasy/webapp/processor/EsFactory.java
@@ -1,23 +1,12 @@
 package io.mkeasy.webapp.processor;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import javax.servlet.ServletContext;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.ParseException;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.elasticsearch.client.Request;
-import org.elasticsearch.client.Response;
-import org.elasticsearch.client.RestClient;
+// import org.elasticsearch.client.Request;
+// import org.elasticsearch.client.Response;
+// import org.elasticsearch.client.RestClient;
 
-import com.hubspot.jinjava.Jinjava;
-
-import io.mkeasy.resolver.CommandMap;
-import io.mkeasy.utils.JSONUtil2;
 import io.mkeasy.utils.WebUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -46,39 +35,39 @@ public class EsFactory {
 	@Autowired
 	ServletContext context;
 
-	public String getEsQuery(String path, String fileName) throws IOException {
-		String queryPath = "/WEB-INF/sql/elastic";
-        queryPath += "/"+path+"/"+fileName+".json";
-		InputStream is = context.getResourceAsStream(queryPath);
-		String jsonQuery = IOUtils.toString(is, "UTF-8"); 
-		return jsonQuery;
-	}
-
-	public JSONObject renderEsQuery(String entity, CommandMap commandMap) {
-		Jinjava jinjava = new Jinjava();
-		String template = jinjava.render(entity, commandMap.getEgovMap());
-		JSONObject renderedObj = new JSONObject(template);
-		return renderedObj;
-	}
-	
-	@Autowired(required = false)
-	RestClient restClient;
-	
-	public String performRequest(String method, String endpoint, JSONObject queryObj) throws ParseException, IOException {
-		Request req = new Request(method, endpoint);
-		req.setJsonEntity(queryObj.toString());
-		Response response = restClient.performRequest(req);
-		String res = (EntityUtils.toString(response.getEntity()));
-		JSONObject object = new JSONObject(res);
-		return object.toString(2);
-	}
-
-	public JSONObject getJSONObject(JSONObject innerObj, String key) {
-		return JSONUtil2.getJSONObject(innerObj, key);
-	}
-
-	public String getValue(JSONObject tmpObj, String key, String defaultValue, boolean roundOk) {
-		return JSONUtil2.getValue(tmpObj, key, defaultValue, roundOk);
-	}
+//	public String getEsQuery(String path, String fileName) throws IOException {
+//		String queryPath = "/WEB-INF/sql/elastic";
+//        queryPath += "/"+path+"/"+fileName+".json";
+//		InputStream is = context.getResourceAsStream(queryPath);
+//		String jsonQuery = IOUtils.toString(is, "UTF-8"); 
+//		return jsonQuery;
+//	}
+//
+//	public JSONObject renderEsQuery(String entity, CommandMap commandMap) {
+//		Jinjava jinjava = new Jinjava();
+//		String template = jinjava.render(entity, commandMap.getEgovMap());
+//		JSONObject renderedObj = new JSONObject(template);
+//		return renderedObj;
+//	}
+//	
+//	@Autowired(required = false)
+//	RestClient restClient;
+//	
+//	public String performRequest(String method, String endpoint, JSONObject queryObj) throws ParseException, IOException {
+//		Request req = new Request(method, endpoint);
+//		req.setJsonEntity(queryObj.toString());
+//		Response response = restClient.performRequest(req);
+//		String res = (EntityUtils.toString(response.getEntity()));
+//		JSONObject object = new JSONObject(res);
+//		return object.toString(2);
+//	}
+//
+//	public JSONObject getJSONObject(JSONObject innerObj, String key) {
+//		return JSONUtil2.getJSONObject(innerObj, key);
+//	}
+//
+//	public String getValue(JSONObject tmpObj, String key, String defaultValue, boolean roundOk) {
+//		return JSONUtil2.getValue(tmpObj, key, defaultValue, roundOk);
+//	}
 
 }
